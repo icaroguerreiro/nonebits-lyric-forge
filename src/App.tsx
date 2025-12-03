50a47156951d68ed06d92ce3142ceece027501c8
Ícaro <icaroguerreirocosta@gmail.com>
2025-12-03 00:23:07 -0300

    Refactor Home page layout and enhance theme
    Replaces Card-based layout with MUI Grid containers for improved spacing and structure on the Home page. Updates theme in App.tsx to include custom dark mode, global background, and component style overrides for better UI consistency.

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,59 @@ import { AppRoutes } from "./routes/AppRoutes";
 import { WrapperProvider } from "./contexts/wrapper";
 import { ToastProvider } from "./contexts/toast";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { Padding } from "@mui/icons-material";
 
-const darkTheme = createTheme({
+const customTheme = createTheme({
   palette: {
     mode: "dark",
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          backgroundColor: "#333 !important", // <-- sua cor aqui
+          color: "#fff", // opcional, caso queira texto claro
+          padding: "1rem",
+        },
+      },
+    },
+    MuiAccordion: {
+      styleOverrides: {
+        root: {
+          margin: "8px 0 !important",
+          "&:before": {
+            display: "none",
+          },
+          "&.Mui-expanded": {
+            margin: "8px 0 !important",
+          },
+        },
+      },
+    },
+    MuiAccordionSummary: {
+      styleOverrides: {
+        root: {
+          minHeight: "60px !important",
+          "&.Mui-expanded": {
+            minHeight: "60px !important",
+          },
+        },
+        content: {
+          margin: "6px 0",
+          "&.Mui-expanded": {
+            margin: "6px 0",
+          },
+        },
+      },
+    },
+    MuiAccordionDetails: {
+      styleOverrides: {
+        root: {
+          padding: "12px 16px",
+        },
+      },
+    },
   },
   typography: {
     fontSize: 12,
@@ -15,7 +64,8 @@ const darkTheme = createTheme({
 
 function App() {
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
       <WrapperProvider>
         <ToastProvider>
           <AppRoutes />
